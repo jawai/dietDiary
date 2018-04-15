80609e3330dcc624f98818152ccfe6f42ebcd134
jwai <jason.a.wai@gmail.com>
2018-04-15 02:05:40 -0400

    fix time zone on local

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module DietDiary
   class Application < Rails::Application
-    config.time_zone = 'Eastern Time (US & Canada)'
     config.active_job.queue_adapter = :sidekiq
     config.application_name = Rails.application.class.parent_name
     # Initialize configuration defaults for originally generated Rails version.
@@ -17,5 +16,7 @@ module DietDiary
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
   end
 end
